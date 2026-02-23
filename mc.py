6a69239e02586379b2643d404b2d6773bebc76b4
Troy Dijsselbloem <troy.dijsselbloem@ru.nl>
2026-02-23 17:00:33 +0100

    feat: prediction montecarlo

--- mc.py
+++ mc.py
@@ -1,16 +1,19 @@
 from environment import MouseEnv
 
-def montecarlo_prediction(policy: dict[int, int], env: MouseEnv, num_of_episodes: int, gamma: float = 1.0):
-    values: dict[float] = {state: 0.0 for state in range(env.num_of_states)} # type:ignore
-    returns = [[] for _ in range(env.num_of_states)] # List of lists to store returns for each state
+def montecarlo_prediction(policy: dict[int, int], env: MouseEnv, num_of_episodes: int, gamma: float = 0.5):
+    returns: dict[int, list[float]] = {state : [] for state in range(env.num_of_states)}
 
     for _ in range(num_of_episodes):
         episode = generate_episode(policy, env)
         g = 0
-        for state, action, reward in episode[::-1]:
-            pass
+        while episode:
+            state, action, reward = episode.pop()
+            g = gamma * g + reward
+            if state not in episode:
+                returns[state].append(g)
+                break
 
-
+    values: dict[int, float] = {state : sum(returns[state]) / len(returns[state]) for state in range(env.num_of_states)}
     return values
 
 def generate_episode(policy, env: MouseEnv) -> list[tuple[int, int, float]]:
