25e73beb6f12f18d17b910dd53ba75d35bcdd41c
Julian <113358009+skywqlkered@users.noreply.github.com>
2026-03-09 10:37:05 +0100

    created sarsa plot and removed env dependency
    fix: changed env to MouseEnv in sarsa functions
    feat: added SARSA policy plot to notebook

--- td.py
+++ td.py
@@ -2,7 +2,7 @@ from environment import MouseEnv
 import numpy as np
 
 
-def epsilon_greedy(state: int, epsilon: float, Qtable: np.typing.NDArray, env) -> int:
+def epsilon_greedy(state: int, epsilon: float, Qtable: np.typing.NDArray) -> int:
     """Returns an action based on the epsilon value
 
     Args:
@@ -14,18 +14,17 @@ def epsilon_greedy(state: int, epsilon: float, Qtable: np.typing.NDArray, env) -
 
     """
     if np.random.random() < epsilon:
-        return np.random.randint(low=0, high=env.num_of_actions)
+        return np.random.randint(low=0, high=MouseEnv.num_of_actions)
     else:
         return int(np.argmax(Qtable[state]))
 
 
 def run_SARSA(
-    env: MouseEnv,
     num_of_episodes: int,
     learning_rate: float,
     discount_rate: float,
     epsilon: float,
-) -> np.typing.NDArray:
+) -> tuple[np.typing.NDArray, int]:
     """Runs the SARSA learning algorithm and returns Q-values
 
     Args:
@@ -39,25 +38,23 @@ def run_SARSA(
         Q-values (np.typing.NDArray): A table of Q values of state and action
 
     """
-    Q = np.zeros((env.num_of_states, env.num_of_actions))
+    Q = np.zeros((MouseEnv.num_of_states, MouseEnv.num_of_actions))
 
     for _ in range(num_of_episodes):
         steps = 0
-        state = np.random.randint(0, env.num_of_states)
-        action = epsilon_greedy(state=state, epsilon=epsilon, Qtable=Q, env=env)
+        state = np.random.randint(0, MouseEnv.num_of_states)
+        action = epsilon_greedy(state=state, epsilon=epsilon, Qtable=Q)
         total_reward = 0
         done = False
 
         while not done:
 
-            done = env.is_terminal_obs(state)
-            reward = env.get_reward(
+            done = MouseEnv.is_terminal_obs(state)
+            reward = MouseEnv.get_reward(
                 state, action
             )  # reward of  taking action in current state
-            next_state = env.get_next_state(state, action=action)
-            next_action = epsilon_greedy(
-                state=next_state, epsilon=epsilon, Qtable=Q, env=env
-            )
+            next_state = MouseEnv.get_next_state(state, action=action)
+            next_action = epsilon_greedy(state=next_state, epsilon=epsilon, Qtable=Q)
 
             TD_target = reward + discount_rate * Q[next_state, next_action]
             TD_error = TD_target - Q[state, action]
@@ -68,17 +65,19 @@ def run_SARSA(
 
             steps += 1
             total_reward += reward
-    return Q
+    try:
+        return Q, steps  # type: ignore
+    except Exception:
+        raise ValueError("Maybe set the num of episodes to something thats not 0...")
 
 
-def SARSA(env, num_of_episodes, alpha, discount, epsilon):
+def SARSA(num_of_episodes, alpha, discount, epsilon):
     """Runs the sarsa learning algorithm and returns a policy
 
     Returns:
         policy (dict): mapping of state to action
     """
-    Q_sarsa = run_SARSA(
-        env,
+    Q_sarsa, steps = run_SARSA(
         num_of_episodes,
         learning_rate=alpha,
         discount_rate=discount,
@@ -88,16 +87,15 @@ def SARSA(env, num_of_episodes, alpha, discount, epsilon):
     policy = {}
     for i, value in enumerate(SARSA_policy):
         policy[i] = int(value)
-    return policy
+    return policy, Q_sarsa, steps
 
 
 def sarsinator():
-    env = MouseEnv()
     episodes = 10000
     alpha = 0.5
     discount = 0.5
     epsilon = 0.0
-    policy = SARSA(env, episodes, alpha, discount, epsilon)
+    policy, Q_sarsa, steps = SARSA(episodes, alpha, discount, epsilon)
     print(policy)
 
 
